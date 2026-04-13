6cfc2585da3834d04f53ba2b1456d6940d338984
Jeison <jsolorzano26@aoiths.org>
2026-04-13 13:39:39 -0400

    Complete

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,32 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoianNvbG9yemFubzI2IiwiYSI6ImNtbnJ4YmU1YzA2eGkycG9kdjFoYmlkaGYifQ.PXQC5uWI9PPdFpArqYEB9A';
 
-mapboxgl.accessToken = ' ACCESS TOKEN ';
+
+var map = new mapboxgl.Map({
+   container: 'map',
+   style: 'mapbox://styles/mapbox/streets-v12',
+   center: [-73.97, 40.75],
+   zoom: 9
+});
 
 
-var mapProps = {
-	container: 'map',
-	center: [-73.97, 40.75],
-	zoom: 9
-};
+map.doubleClickZoom.disable();
 
-var map = new mapboxgl.Map(mapProps);
 
-map.on(" event ", function(evt){
+let counter = 0;
 
-	
-	
+
+const inputBox = document.getElementById("info");
+
+
+inputBox.value = "Clicks: 0";
+
+
+map.on("click", function () {
+   counter += 1;
+   inputBox.value = "Clicks: " + counter;
+});
+
+map.on("dblclick", function () {
+   counter += 4;
+   inputBox.value = "Clicks: " + counter;
 });
